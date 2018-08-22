a8eae44d660f39ee6f76b9a311be1d38a2ace2cc
za550u <42606177+za550u@users.noreply.github.com>
2018-08-22 19:44:12 +0900

    最初
    最初です

---
+++ HitNombers/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);  // 不死身
+    }
+
+
+    // Use this for initialization
+    void Start () {
+        SceneManager.LoadScene("Scenes/Title/Title");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
